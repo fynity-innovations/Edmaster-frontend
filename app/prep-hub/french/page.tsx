ca98b9e0b92919be34d40471faf2042c6966b398
fynity-innovations <fynityinnovations@gmail.com>
2026-01-04 23:07:08 +0530

    boom

---
+++ app/prep-hub/french/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function French() {
+
+  return (
+    <div className="min-h-screen pt-24 pb-16">
+      <p>Page is under working</p>
+    </div>
+  )
+}
---
+++ app/prep-hub/toefl/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function Toefl() {
+
+  return (
+    <div className="min-h-screen pt-24 pb-16">
+      <p>Page is under working</p>
+    </div>
+  )
+}
